14654581854e424878315ec01e0e6197784015b4
Arthur Lockman <hello@rthr.me>
2016-12-26 14:21:41 -0500

    Added FRC API connection in NodeJS

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
-var http = require('http')
-  , fs   = require('fs')
-  , url  = require('url')
+var http    = require('http')
+  , fs      = require('fs')
+  , url     = require('url')
   , express = require('express')
-  , app  = express()
-  , router = express.Router()
+  , app     = express()
+  , router  = express.Router()
+  , unirest = require('unirest')
+
+var token = require("./token.json")
 
 var server = app.listen(8080, function () {
     var host = server.address().address
@@ -13,7 +16,16 @@ var server = app.listen(8080, function () {
 
 app.use('/api', router)
 
+router.route('/:year/events').get(function(req, res) {
+    unirest.get('https://frc-api.firstinspires.org/v2.0/'+req.params.year+'/events')
+        .headers({'Authorization': token.token})
+        .end(function (response) {
+        res.writeHead(200, {'Content-type': 'text/html'})
+        res.end(JSON.stringify(response.body), 'utf-8')         
+    })
+})
+
 app.get('/', function(req, res) {
     res.writeHead(200, {'Content-type': 'text/html'})
-    res.end('Hello!', 'utf-8')
+    res.end('Welcome to FRC events!', 'utf-8')
 })
