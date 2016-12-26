14bb60c0181a60d1a1bff2c43c672c54317a7f5d
Arthur Lockman <hello@rthr.me>
2016-12-26 16:20:34 -0500

    Added loading event listing

--- scripts.js
+++ scripts.js
@@ -1,8 +1,12 @@
+var eventsData = {}
+
 window.onload = function() {
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
+        $('.selectpicker').selectpicker('mobile')
+    }
     loadEventsList()
     document.getElementById('yearPicker').onchange = function() {
         var e = document.getElementById('yearPicker')
-        console.log(e.options[e.selectedIndex].value)
         loadEventsList()
     }
 }
@@ -13,7 +17,20 @@ function loadEventsList() {
     var req = new XMLHttpRequest()
     req.open('GET', '/api/' + e.options[e.selectedIndex].value + '/events')
     req.addEventListener('load', function() {
-        document.getElementById('textArea').innerHTML = req.responseText
+        var tmp = JSON.parse(req.responseText).Events
+        var options = []
+        for (i = 0; i < tmp.length; i++)
+        {
+            var _option = {text: tmp[i].name, value: tmp[i].code}
+            options.push(_option)
+        }
+        var sel = $('#eventSelector')
+        sel.empty()
+        $.each(options, function(index, option) {
+            sel.append($('<option></option>')
+            .attr('value', option.value).text(option.text))
+        })
+        sel.selectpicker('refresh')
     })
     req.send()
 }
