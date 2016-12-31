1145534865e45391e09223ce27b07b343581c309
James Lockman <jlockman@adobe.com>
2016-12-31 14:44:44 -0500

    Fixed a bunch of JS formatting issues

--- scripts.js
+++ scripts.js
@@ -1,226 +1,231 @@
-$.getScript('https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.17.1/moment.min.js', function(){});
+/*global moment */
+
+$.getScript('https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.17.1/moment.min.js', function () {});
 
 window.onload = function () {
-	"use strict";
-	document.getElementById('scheduleProgressBar').style.display = 'none';
-	if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
-		$('.selectpicker').selectpicker('mobile');
-	}
-	loadEventsList();
-	document.getElementById('yearPicker').onchange = function () {
-		loadEventsList();
-	};
-	document.getElementById('eventSelector').onchange = function () {
-		handleEventSelection();
-	};
-	document.getElementById('setupTabPicker').click();
+    "use strict";
+    document.getElementById('scheduleProgressBar').style.display = 'none';
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
+        $('.selectpicker').selectpicker('mobile');
+    }
+    loadEventsList();
+    document.getElementById('yearPicker').onchange = function () {
+        loadEventsList();
+    };
+    document.getElementById('eventSelector').onchange = function () {
+        handleEventSelection();
+    };
+    document.getElementById('setupTabPicker').click();
 };
 
+
 function handleEventSelection() {
-	"use strict";
-	document.getElementById('scheduleProgressBar').style.display = 'block';
-	document.getElementById('scheduleContainer').innerHTML = 'Select an Event';
-	var e = document.getElementById('eventSelector');
-	var data = JSON.parse(e.value);
-	var codeText = document.getElementById('eventCodeContainer');
-	var locationText = document.getElementById('eventLocationContainer');
-	var dateText = document.getElementById('eventDateContainer');
-	codeText.innerHTML = data.code;
-	locationText.innerHTML = data.venue + " in " + data.city + ", " + data.stateprov + " " + data.country;
-	var startDate = moment(data.dateStart, 'YYYY-MM-DDTHH:mm:ss').format('MMMM Do');
-	var endDate = moment(data.dateEnd, 'YYYY-MM-DDTHH:mm:ss').format('MMMM Do, YYYY');
-	dateText.innerHTML = startDate + " to " + endDate;
-	getHybridSchedule();
-	getTeamList();
+    "use strict";
+    document.getElementById('scheduleProgressBar').style.display = 'block';
+    document.getElementById('scheduleContainer').innerHTML = 'Select an Event';
+    var e = document.getElementById('eventSelector');
+    var data = JSON.parse(e.value);
+    var codeText = document.getElementById('eventCodeContainer');
+    var locationText = document.getElementById('eventLocationContainer');
+    var dateText = document.getElementById('eventDateContainer');
+    codeText.innerHTML = data.code;
+    locationText.innerHTML = data.venue + " in " + data.city + ", " + data.stateprov + " " + data.country;
+    var startDate = moment(data.dateStart, 'YYYY-MM-DDTHH:mm:ss').format('MMMM Do');
+    var endDate = moment(data.dateEnd, 'YYYY-MM-DDTHH:mm:ss').format('MMMM Do, YYYY');
+    dateText.innerHTML = startDate + " to " + endDate;
+    getHybridSchedule();
+    getTeamList();
 }
 
 function loadEventsList() {
-	"use strict";
-	var e = document.getElementById('yearPicker');
-	var req = new XMLHttpRequest();
-	req.open('GET', '/api/' + e.options[e.selectedIndex].value + '/events');
-	req.addEventListener('load', function () {
-		var tmp = JSON.parse(req.responseText).Events;
-		var options = [];
-		for (var i = 0; i < tmp.length; i++) {
-			var _option = {
-				text: tmp[i].name,
-				value: tmp[i]
-			};
-			options.push(_option);
-		}
-		options.sort(function (a, b) {
-			if (a.text < b.text) {
-				return -1;
-			}
-			if (a.text > b.text) {
-				return 1;
-			}
-			return 0;
-		});
-		var sel = $('#eventSelector');
-		sel.empty();
-		$.each(options, function (index, option) {
-			sel.append($('<option></option>')
-				.attr('value', JSON.stringify(option.value)).text(option.text));
-		});
-		sel.selectpicker('refresh');
-		handleEventSelection();
-	});
-	req.send();
+    "use strict";
+    var e = document.getElementById('yearPicker');
+    var req = new XMLHttpRequest();
+    req.open('GET', '/api/' + e.options[e.selectedIndex].value + '/events');
+    req.addEventListener('load', function () {
+        var tmp = JSON.parse(req.responseText).Events;
+        var options = [];
+        for (var i = 0; i < tmp.length; i++) {
+            var _option = {
+                text: tmp[i].name,
+                value: tmp[i]
+            };
+            options.push(_option);
+        }
+        options.sort(function (a, b) {
+            if (a.text < b.text) {
+                return -1;
+            }
+            if (a.text > b.text) {
+                return 1;
+            }
+            return 0;
+        });
+        var sel = $('#eventSelector');
+        sel.empty();
+        $.each(options, function (index, option) {
+            sel.append($('<option></option>')
+                .attr('value', JSON.stringify(option.value)).text(option.text));
+        });
+        sel.selectpicker('refresh');
+        handleEventSelection();
+    });
+    req.send();
 }
 
 function openTab(evt, cityName) {
-	"use strict";
-	// Declare all variables
-	var i, tabcontent, tablinks;
+    "use strict";
+    // Declare all variables
+    var i, tabcontent, tablinks;
 
-	// Get all elements with class="tabcontent" and hide them
-	tabcontent = document.getElementsByClassName("tabcontent");
-	for (i = 0; i < tabcontent.length; i++) {
-		tabcontent[i].style.display = "none";
-	}
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
 
-	// Get all elements with class="tablinks" and remove the class "active"
-	tablinks = document.getElementsByClassName("tablinks");
-	for (i = 0; i < tablinks.length; i++) {
-		tablinks[i].className = tablinks[i].className.replace(" active", "");
-	}
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
 
-	// Show the current tab, and add an "active" class to the link that opened the tab
-	document.getElementById(cityName).style.display = "block";
-	evt.currentTarget.className += " active";
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
 }
 
 function getHybridSchedule() {
-	"use strict";
-	var year = document.getElementById('yearPicker');
-	var eventPicker = document.getElementById('eventSelector');
-	var req = new XMLHttpRequest();
-	req.open('GET', '/api/' + year.options[year.selectedIndex].value + '/schedule/' + JSON.parse(eventPicker.options[eventPicker.selectedIndex].value).code + '/qual');
-	req.addEventListener('load', function () {
-		var data = JSON.parse(req.responseText);
-		if (data.Schedule.length === 0) {
-			document.getElementById('scheduleContainer').innerHTML = '<b>No qualification matches have been scheduled for this event.</b>';
-		} else {
-			document.getElementById('scheduleContainer').innerHTML = '<div class=""><table id="scheduleTable" class="table table-bordered">';
-			document.getElementById('scheduleTable').innerHTML += '<thead><tr><td><b>Time</b></td><td><b>Description</b></td><td><b>Match Number</b></td><td><b>Red 1</b></td><td><b>Red 2</b></td><td><b>Red 3</b></td><td><b>Red 4</b></td><td><b>Blue 1</b></td><td><b>Blue 2</b></td><td><b>Blue 3</b></td><td><b>Blue 4</b></td></tr></thead>';
-			for (var i = 0; i < data.Schedule.length; i++) {
-				var element = data.Schedule[i];
-				document.getElementById('scheduleTable').innerHTML += generateMatchTableRow(element);
-			}
-		}
-		req1.send();
-	});
-	var req1 = new XMLHttpRequest();
-	req1.open('GET', '/api/' + year.options[year.selectedIndex].value + '/schedule/' + JSON.parse(eventPicker.options[eventPicker.selectedIndex].value).code + '/playoff');
-	req1.addEventListener('load', function () {
-		console.log(JSON.stringify(req1));
-		var data = JSON.parse(req1.responseText);
-		if (data.Schedule.length === 0) {
-			document.getElementById('scheduleContainer').innerHTML += '</table><p><b>No playoff matches have been scheduled for this event.</b></p>';
-		} else {
-			for (var i = 0; i < data.Schedule.length; i++) {
-				var element = data.Schedule[i];
-				document.getElementById('scheduleTable').innerHTML += generateMatchTableRow(element);
-			}
-			document.getElementById('scheduleContainer').innerHTML += '</table></div>';
-		}
-		document.getElementById('scheduleProgressBar').style.display = 'none';
-	});
-	req.send();
+    "use strict";
+    var year = document.getElementById('yearPicker');
+    var eventPicker = document.getElementById('eventSelector');
+    var req = new XMLHttpRequest();
+    req.open('GET', '/api/' + year.options[year.selectedIndex].value + '/schedule/' + JSON.parse(eventPicker.options[eventPicker.selectedIndex].value).code + '/qual');
+    req.addEventListener('load', function () {
+        var data = JSON.parse(req.responseText);
+        if (data.Schedule.length === 0) {
+            document.getElementById('scheduleContainer').innerHTML = '<b>No qualification matches have been scheduled for this event.</b>';
+        } else {
+            document.getElementById('scheduleContainer').innerHTML = '<div class=""><table id="scheduleTable" class="table table-bordered">';
+            document.getElementById('scheduleTable').innerHTML += '<thead><tr><td><b>Time</b></td><td><b>Description</b></td><td><b>Match Number</b></td><td><b>Red 1</b></td><td><b>Red 2</b></td><td><b>Red 3</b></td><td><b>Red 4</b></td><td><b>Blue 1</b></td><td><b>Blue 2</b></td><td><b>Blue 3</b></td><td><b>Blue 4</b></td></tr></thead>';
+            for (var i = 0; i < data.Schedule.length; i++) {
+                var element = data.Schedule[i];
+                document.getElementById('scheduleTable').innerHTML += generateMatchTableRow(element);
+            }
+        }
+        req1.send();
+    });
+    var req1 = new XMLHttpRequest();
+    req1.open('GET', '/api/' + year.options[year.selectedIndex].value + '/schedule/' + JSON.parse(eventPicker.options[eventPicker.selectedIndex].value).code + '/playoff');
+    req1.addEventListener('load', function () {
+        console.log(JSON.stringify(req1));
+        var data = JSON.parse(req1.responseText);
+        if (data.Schedule.length === 0) {
+            document.getElementById('scheduleContainer').innerHTML += '</table><p><b>No playoff matches have been scheduled for this event.</b></p>';
+        } else {
+            for (var i = 0; i < data.Schedule.length; i++) {
+                var element = data.Schedule[i];
+                document.getElementById('scheduleTable').innerHTML += generateMatchTableRow(element);
+            }
+            document.getElementById('scheduleContainer').innerHTML += '</table></div>';
+        }
+        document.getElementById('scheduleProgressBar').style.display = 'none';
+    });
+    req.send();
 }
 
 function getTeamList() {
-	"use strict";
-	var year = document.getElementById('yearPicker');
-	var eventPicker = document.getElementById('eventSelector');
-	var req2 = new XMLHttpRequest();
-	req2.open('GET', '/api/' + year.options[year.selectedIndex].value + '/teams/' + JSON.parse(eventPicker.options[eventPicker.selectedIndex].value).code);
-	req2.addEventListener('load', function () {
-		var data = JSON.parse(req2.responseText);
-		if (data.teams.length === 0) {
-			document.getElementById('teamsContainer').innerHTML = '<b>No teams have registered for this event.</b>';
-		} else {
-			document.getElementById('teamsContainer').innerHTML = '<div class=""><table id="teamsTable" class="table table-bordered">';
-			document.getElementById('teamsTable').innerHTML += '<thead><tr><td><b>Number</b></td><td><b>Short Name</b></td><td><b>City</b></td><td><b>Sponsors</b></td><td><b>Organization</b></td><td><b>Rookie Year</b></td><td><b>Robot name</b></td></tr></thead>';
-			for (var i = 0; i < data.teams.length; i++) {
-				var element = data.teams[i];
-				document.getElementById('teamsTable').innerHTML += generateTeamTableRow(element);
-			}
-		}
-		document.getElementById('teamProgressBar').style.display = 'none';
-
-	});
-	req2.send();
+    "use strict";
+    var year = document.getElementById('yearPicker');
+    var eventPicker = document.getElementById('eventSelector');
+    var req2 = new XMLHttpRequest();
+    req2.open('GET', '/api/' + year.options[year.selectedIndex].value + '/teams/' + JSON.parse(eventPicker.options[eventPicker.selectedIndex].value).code);
+    req2.addEventListener('load', function () {
+        var data = JSON.parse(req2.responseText);
+        if (data.teams.length === 0) {
+            document.getElementById('teamsContainer').innerHTML = '<b>No teams have registered for this event.</b>';
+        } else {
+            document.getElementById('teamsContainer').innerHTML = '<div class="" style="display:table;"><table id="teamsTable" class="table table-bordered">';
+            document.getElementById('teamsTable').innerHTML += '<thead><tr><td><b>Number</b></td><td><b>Short Name</b></td><td><b>City</b></td><td><b>Sponsors</b></td><td><b>Organization</b></td><td><b>Rookie Year</b></td><td><b>Robot name</b></td></tr></thead>';
+            document.getElementById('teamsTable').innerHTML += '<tbody>';
+            for (var i = 0; i < data.teams.length; i++) {
+                var element = data.teams[i];
+                document.getElementById('teamsTable').innerHTML += generateTeamTableRow(element);
+            }
+            document.getElementById('teamsTable').innerHTML += '</tbody>';
+            document.getElementById('teamsTable').innerHTML += "</table></div>";
+        }
+        document.getElementById('teamProgressBar').style.display = 'none';
+    });
+    req2.send();
 }
 
 // UTILITY FUNCTIONS
 function syntaxHighlight(json) {
-	"use strict";
-	if (typeof json !== 'string') {
-		json = JSON.stringify(json, undefined, 2);
-	}
-	json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-	return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
-		var cls = 'number';
-		if (/^"/.test(match)) {
-			if (/:$/.test(match)) {
-				cls = 'key';
-			} else {
-				cls = 'string';
-			}
-		} else if (/true|false/.test(match)) {
-			cls = 'boolean';
-		} else if (/null/.test(match)) {
-			cls = 'null';
-		}
-		return '<span class="' + cls + '">' + match + '</span>';
-	});
+    "use strict";
+    if (typeof json !== 'string') {
+        json = JSON.stringify(json, undefined, 2);
+    }
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+        var cls = 'number';
+        if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+                cls = 'key';
+            } else {
+                cls = 'string';
+            }
+        } else if (/true|false/.test(match)) {
+            cls = 'boolean';
+        } else if (/null/.test(match)) {
+            cls = 'null';
+        }
+        return '<span class="' + cls + '">' + match + '</span>';
+    });
 }
 
 function generateMatchTableRow(matchData) {
-	"use strict";
-	var returnData = '<tr><td>';
-	returnData += moment(matchData.startTime, 'YYYY-MM-DDTHH:mm:ss').format('ddd hh:mm:ss A') + '</td><td>';
-	returnData += matchData.description + '</td><td>';
-	returnData += matchData.matchNumber + '</td><td>';
-	returnData += getTeamForStation(matchData.Teams, 'Red1').teamNumber + '</td><td>';
-	returnData += getTeamForStation(matchData.Teams, 'Red2').teamNumber + '</td><td>';
-	returnData += getTeamForStation(matchData.Teams, 'Red3').teamNumber + '</td><td>';
-	returnData += getTeamForStation(matchData.Teams, 'Red4').teamNumber + '</td><td>';
-	returnData += getTeamForStation(matchData.Teams, 'Blue1').teamNumber + '</td><td>';
-	returnData += getTeamForStation(matchData.Teams, 'Blue2').teamNumber + '</td><td>';
-	returnData += getTeamForStation(matchData.Teams, 'Blue3').teamNumber + '</td><td>';
-	returnData += getTeamForStation(matchData.Teams, 'Blue4').teamNumber + '</td>';
-	return returnData + '</tr>';
+    "use strict";
+    var returnData = '<tr><td>';
+    returnData += moment(matchData.startTime, 'YYYY-MM-DDTHH:mm:ss').format('ddd hh:mm:ss A') + '</td><td>';
+    returnData += matchData.description + '</td><td>';
+    returnData += matchData.matchNumber + '</td><td>';
+    returnData += getTeamForStation(matchData.Teams, 'Red1').teamNumber + '</td><td>';
+    returnData += getTeamForStation(matchData.Teams, 'Red2').teamNumber + '</td><td>';
+    returnData += getTeamForStation(matchData.Teams, 'Red3').teamNumber + '</td><td>';
+    returnData += getTeamForStation(matchData.Teams, 'Red4').teamNumber + '</td><td>';
+    returnData += getTeamForStation(matchData.Teams, 'Blue1').teamNumber + '</td><td>';
+    returnData += getTeamForStation(matchData.Teams, 'Blue2').teamNumber + '</td><td>';
+    returnData += getTeamForStation(matchData.Teams, 'Blue3').teamNumber + '</td><td>';
+    returnData += getTeamForStation(matchData.Teams, 'Blue4').teamNumber + '</td>';
+    return returnData + '</tr>';
 }
 
 function getTeamForStation(teamList, station) {
-	"use strict";
-	for (var j = 0; j < teamList.length; j++) {
-		if (teamList[j].station === station) {
-			return teamList[j];
-		}
-	}
-	var r = {};
-	r.teamNumber = "";
-	return r;
+    "use strict";
+    for (var j = 0; j < teamList.length; j++) {
+        if (teamList[j].station === station) {
+            return teamList[j];
+        }
+    }
+    var r = {};
+    r.teamNumber = "";
+    return r;
 }
 
 function generateTeamTableRow(teamData) {
-	"use strict";
-	var returnData = '<tr><td>';
-	returnData += teamData.teamNumber + '</td><td>';
-	returnData += teamData.nameShort + '</td><td>';
-	returnData += teamData.city + ", " + teamData.stateProv + '</td><td>';
-	returnData += teamData.nameFull + '</td><td>';
-	returnData += teamData.nameShort + '</td><td>'; //replace with organization function.
-	returnData += teamData.rookieYear + '</td><td>';
-	if (teamData.robotName === null) {
-		returnData += "No robot name reported" + '</td>';
-	} else {
-		returnData += teamData.robotName + '</td>';
-	}
-	return returnData + '</tr>';
+    "use strict";
+    var returnData = '<tr><td>';
+    returnData += teamData.teamNumber + '</td><td>';
+    returnData += teamData.nameShort + '</td><td>';
+    returnData += teamData.city + ", " + teamData.stateProv + '</td><td>';
+    returnData += teamData.nameFull + '</td><td>';
+    returnData += teamData.nameShort + '</td><td>'; //replace with organization function.
+    returnData += teamData.rookieYear + '</td><td>';
+    if (teamData.robotName === null) {
+        returnData += "No robot name reported" + '</td>';
+    } else {
+        returnData += teamData.robotName + '</td>';
+    }
+    return returnData + '</tr>';
 }
--- server.js
+++ server.js
@@ -1,66 +1,90 @@
-var http    = require('http')
-  , fs      = require('fs')
-  , url     = require('url')
-  , express = require('express')
-  , app     = express()
-  , router  = express.Router()
-  , unirest = require('unirest')
+/*global require */
 
-var token = require("./token.json")
+var http = require('http'),
+    fs = require('fs'),
+    url = require('url'),
+    express = require('express'),
+    app = express(),
+    router = express.Router(),
+    unirest = require('unirest');
+
+var token = require("./token.json");
 
 var server = app.listen(8080, function () {
-    var host = server.address().address
-    var port = server.address().port
-    console.log('Server listening at http://%s:%s', host, port)
-})
+    'use strict';
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Server listening at http://%s:%s', host, port);
+});
 
 function sendFile(res, filename, contentType) {
-  contentType = contentType || 'text/html'
+    'use strict';
+    contentType = contentType || 'text/html';
 
-  fs.readFile(filename, function(error, content) {
-    res.writeHead(200, {'Content-type': contentType})
-    res.end(content, 'utf-8')
-  })
+    fs.readFile(filename, function (error, content) {
+        res.writeHead(200, {
+            'Content-type': contentType
+        });
+        res.end(content, 'utf-8');
+    });
 }
 
-app.use('/api', router)
+app.use('/api', router);
 
-router.route('/:year/events').get(function(req, res) {
-    unirest.get('https://frc-api.firstinspires.org/v2.0/'+req.params.year+'/events')
-        .headers({'Authorization': token.token})
+router.route('/:year/events').get(function (req, res) {
+    'use strict';
+    unirest.get('https://frc-api.firstinspires.org/v2.0/' + req.params.year + '/events')
+        .headers({
+            'Authorization': token.token
+        })
         .end(function (response) {
-        res.writeHead(200, {'Content-type': 'text/html'})
-        res.end(JSON.stringify(response.body), 'utf-8')         
-    })
-})
+            res.writeHead(200, {
+                'Content-type': 'text/html'
+            });
+            res.end(JSON.stringify(response.body), 'utf-8');
+        });
+});
 
-router.route('/:year/schedule/:eventCode/:tlevel').get(function(req, res) {
-    unirest.get('https://frc-api.firstinspires.org/v2.0/'+req.params.year+'/schedule/'+req.params.eventCode+'/'+req.params.tlevel+'/hybrid')
-        .headers({'Authorization': token.token})
+router.route('/:year/schedule/:eventCode/:tlevel').get(function (req, res) {
+    'use strict';
+    unirest.get('https://frc-api.firstinspires.org/v2.0/' + req.params.year + '/schedule/' + req.params.eventCode + '/' + req.params.tlevel + '/hybrid')
+        .headers({
+            'Authorization': token.token
+        })
         .end(function (response) {
-            res.writeHead(200, {'Content-type': 'text/html'})
-            res.end(JSON.stringify(response.body), 'utf-8')
-    })
-})
+            res.writeHead(200, {
+                'Content-type': 'text/html'
+            });
+            res.end(JSON.stringify(response.body), 'utf-8');
+        });
+});
 
-router.route('/:year/teams/:eventCode/').get(function(req, res) {
-    unirest.get('https://frc-api.firstinspires.org/v2.0/'+req.params.year+'/teams/?eventcode='+req.params.eventCode)
-        .headers({'Authorization': token.token})
+router.route('/:year/teams/:eventCode/').get(function (req, res) {
+    'use strict';
+    unirest.get('https://frc-api.firstinspires.org/v2.0/' + req.params.year + '/teams/?eventcode=' + req.params.eventCode)
+        .headers({
+            'Authorization': token.token
+        })
         .end(function (response) {
-            res.writeHead(200, {'Content-type': 'text/html'})
-            res.end(JSON.stringify(response.body), 'utf-8')
-    })
-})
+            res.writeHead(200, {
+                'Content-type': 'text/html'
+            });
+            res.end(JSON.stringify(response.body), 'utf-8');
+        });
+});
 
 
-app.get('/', function(req, res) {
-    sendFile(res, 'index.html', 'text/html')
-})
+app.get('/', function (req, res) {
+    'use strict';
+    sendFile(res, 'index.html', 'text/html');
+});
 
-app.get('/scripts.js', function(req, res) {
-    sendFile(res, 'scripts.js', 'text/js')
-})
+app.get('/scripts.js', function (req, res) {
+    'use strict';
+    sendFile(res, 'scripts.js', 'text/js');
+});
 
-app.get('/css/style.css', function(req, res) {
-    sendFile(res, 'style.css', 'text/css')
-})
+app.get('/css/style.css', function (req, res) {
+    'use strict';
+    sendFile(res, 'style.css', 'text/css');
+});
